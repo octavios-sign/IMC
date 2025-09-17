b9a2246dbcf790e173711d63eb414f3891422667
otávio <70035833+octavios-sign@users.noreply.github.com>
2025-09-17 14:31:32 -0300

    corrige os bugs de mapa

--- BlueScreen/src/main/Main.java
+++ BlueScreen/src/main/Main.java
@@ -4,25 +4,22 @@ import javax.swing.JFrame;
 
 public class Main {
 
-	public static void main(String[] args) {
-		
+    public static void main(String[] args) {
 
-	window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	window.setResizable(false);
-	window.setTitle("Presente do Yan");
-	
-	GamePanel gamePanel = new GamePanel();
-	window.add(gamePanel);
-	
-	window.pack();
-	
-	window.setLocationRelativeTo(null);
-	window.setVisible(true);
-	//para abrir uma janela
-	
-	gamePanel.startGameThread();
-	
-	
-	}
+        JFrame window = new JFrame();
 
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Presente do Yan");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        gamePanel.startGameThread();
+    }
 }
